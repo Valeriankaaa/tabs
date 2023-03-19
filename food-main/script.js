2f4709e3d6491ccc50ead657f951a4e076c49fe4
Valeriia <corgiler@gmail.com>
2023-03-19 14:41:13 -0600

    timer

--- food-main/script.js
+++ food-main/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', ()=> {
+    // tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
             tabsContent = document.querySelectorAll('.tabcontent'),
             tabsFolder = document.querySelector('.tabheader');
@@ -30,4 +31,43 @@ window.addEventListener('DOMContentLoaded', ()=> {
             });
         }
     });
+
+    // timer 
+    const timeEnd = '2023-03-31';
+    function getTimeRemaining (finishTime) {
+        const t = Date.parse(finishTime) - Date.parse (new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
+            minutes = Math.floor ((t / 1000 / 60) % 60),
+            seconds = Math.floor ( (t/1000) % 60);
+        return {
+            'total' : t,
+            'days' : days,
+            'hours' : hours,
+            'minutes' : minutes, 
+            'seconds' : seconds
+        }
+    }
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        
+            updateClock();
+            
+        function updateClock() {
+                const time = getTimeRemaining(endTime);
+                days.innerHTML = time.days;
+                hours.innerHTML = time.hours;
+                minutes.innerHTML = time.minutes; 
+                seconds.innerHTML = time.seconds;
+                if( t.total <= 0 ) {
+                    clearInterval(timeInterval);
+                }
+            }
+    }
+    setClock('.timer', timeEnd);
 });
