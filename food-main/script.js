ca66a80a0386a5dba67df43e61f3710f18179c66
Valeriia <corgiler@gmail.com>
2023-03-21 19:19:32 -0600

    Я поняла!!!!!

--- food-main/script.js
+++ food-main/script.js
@@ -59,15 +59,77 @@ window.addEventListener('DOMContentLoaded', ()=> {
             updateClock();
             
         function updateClock() {
-                const time = getTimeRemaining(endTime);
-                days.innerHTML = time.days;
-                hours.innerHTML = time.hours;
-                minutes.innerHTML = time.minutes; 
-                seconds.innerHTML = time.seconds;
+                const t = getTimeRemaining(endTime);
+                days.innerHTML = t.days;
+                hours.innerHTML = t.hours;
+                minutes.innerHTML = t.minutes; 
+                seconds.innerHTML = t.seconds;
                 if( t.total <= 0 ) {
                     clearInterval(timeInterval);
                 }
             }
     }
     setClock('.timer', timeEnd);
+
+    // modal
+    const modalTriger = document.querySelectorAll('[data-modal]'), // кнопка Связаться с нами
+        modal = document.querySelector('.modal'), // попап
+        modalClose = document.querySelector('[data-close]'); // закрыть кнопка
+
+        modalTriger.forEach(btn =>{
+            btn.addEventListener('click', ()=>{
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow='hidden';
+            });
+        });
+
+        // общая функция
+        function closePopUp () {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow='';
+        }
+        
+        // для кнопки закрыть 
+        modalClose.addEventListener ('click', closePopUp);
+        
+        // для пространства вне попап
+        modal.addEventListener ('click', (event) => {
+            if (event.target === modal) {
+                closePopUp ();
+            }
+        });
+
+        // для клавиши escape 
+        document.addEventListener('keydown', (event)=>{
+            if(event.code === 'Escape') {
+                closePopUp ()
+            }
+        });
+
+        // для кнопки закрыть 
+        // // modalClose.addEventListener('click', ()=>{
+        // //     modal.classList.add('hide');
+        // //     modal.classList.remove('show');
+        // //     document.body.style.overflow='';
+        // // });
+        
+        // для пространства вне поп aп
+        // // modal.addEventListener('click', (event)=> {
+        // //     if(event.target === modal) {
+        // //         modal.classList.add('hide');
+        // //         modal.classList.remove('show');
+        // //         document.body.style.overflow='';
+        // //     }
+        // // });
+
+        // для клавиши escape 
+        // // document.addEventListener('keydown', (event)=>{
+        // //     if(event.code === 'Escape') {
+        // //         modal.classList.add('hide');
+        // //         modal.classList.remove('show');
+        // //         document.body.style.overflow='';
+        // //     }
 });
+        
